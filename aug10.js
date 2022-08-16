ad118a6981381509440d83699292f7ab813373cf
JulianBell1 <julianesser@gmail.com>
2022-08-16 10:23:10 -0400

    2022.08.10

--- aug10.js
+++ aug10.js
@@ -151,3 +151,25 @@
           function modifyMultiply (str,loc,num) {
             return Array(num).fill(str.split(' ')[loc]).join('-')
           } 
+
+
+
+//  3.  8kyu
+
+        // Wilson primes satisfy the following condition. Let P represent a prime number.
+
+        // Then,
+
+        // ((P-1)! + 1) / (P * P)
+
+        // should give a whole number.
+
+        // Your task is to create a function that returns true if the given number is a Wilson prime.
+
+        function amIWilson(p) {
+            
+            if (((p-1)! + 1) / (p * p) % 1 == true){
+                return true
+            })
+
+          }
